fe95f67d6ad801c52db3d3e0d4764f8426639435
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-26 16:31:18 -0300

    adiciona javadocs

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -14,30 +14,56 @@ public class GerenciamentoVotacao {
   }
 
 
-
+  /**
+   * @param nome is string, nome da pessoa candidata
+   * @param numero is integer,  numero da pessoa candidata
+   * @author Murilo
+   */
   public void cadastrarPessoaCandidata(String nome, int numero) {
     PessoaCandidata candidato = new PessoaCandidata(nome, numero);
     listPessoasCandidatas.add(candidato);
   }
 
+  /**
+   * @param nome is string, nome da pessoa eleitora
+   * @param numero is integer,  cpf da pessoa eleitora
+   * @author Murilo
+   */
   public void cadastrarPessoaEleitora(String nome, String numero) {
     PessoaEleitora eleitora = new PessoaEleitora(nome, numero);
     listPessoasEleitoras.add(eleitora);
   }
 
+  /**
+   * @param string is cpf da pessoa eleitora
+   * @param numero is integer, numero da pessoa candidatas
+   * @author Murilo
+   */
   public void votar(String string, int numero) {
     // *IMPORTANTE: cada pessoa eleitora só pode votar uma única vez.
 
   }
 
+  /**
+   * Mostrar os resultados atuais da votação, parciais ou não
+   * @author Murilo
+   */
   public void mostrarResultado() {
     // !mostrar resultado da votação
   }
 
+  /**
+   * Retorna lista de Pessoas candidatas
+   * @author Murilo
+   */
   public ArrayList<PessoaCandidata> getListCandidates() {
     return this.listPessoasCandidatas;
   }
 
+  /**
+   * Retorna lista de Pessoas eleitoras
+   * @author Murilo
+   */
   public ArrayList<PessoaEleitora> getListEleitoras() {
     return this.listPessoasEleitoras;
   }
--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -14,6 +14,106 @@ public class Principal {
 
     cadastrarEleitores(votacao);
 
+    iniciarVotação(votacao);
+  }
+
+  /**
+   * Metodo abrir o terminal com opção de criar pessoas Candidatas.
+   * @param scanner instancia do Scanner aberto
+   * @author Murilo
+   */
+  private static short openMenuforPessoaCandidata(Scanner scanner) {
+    short inputNumber = 0;
+    System.out.println("Cadastrar pessoa candidata?");
+    String entrySystemString =
+        "1 - Sim\n2 - Não";
+    System.out.println(entrySystemString);
+    System.out.println("Entre com o número correspondente à opção desejada:");
+
+    inputNumber = scanner.nextShort();
+
+    return inputNumber;
+  }
+
+  /**
+   * Metodo abrir o terminal com opção de criar pessoas Eleitoras.
+   * @param scanner instancia do Scanner aberto
+   * @author Murilo
+   */
+  private static short openMenuforPessoaEleitora(Scanner scanner) {
+    short inputNumber = 0;
+    System.out.println("Cadastrar pessoa eleitora?");
+    String entrySystemString =
+        "1 - Sim\n2 - Não";
+    System.out.println(entrySystemString);
+    System.out.println("Entre com o número correspondente à opção desejada:");
+
+    inputNumber = scanner.nextShort();
+
+    return inputNumber;
+  }
+
+  /**
+   * Metodo abrir o terminal com opção de votação ou ver resultados.
+   * @param scanner instancia do Scanner aberto
+   * @author Murilo
+   */
+  private static short openMenuforVotation(Scanner scanner) {
+    short inputNumber = 0;
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    String entrySystemString =
+        "1 - Votar\n2 - Resultado Parcial\n3 - Finalizar Votação";
+        System.out.println(entrySystemString);
+
+    inputNumber = scanner.nextShort();
+
+    return inputNumber;
+  }
+
+  /**
+   * Metodo para cadastrar pessoas Candidatas.
+   * @param votacao instancia da votação atual
+   * @author Murilo
+   */
+  private static void cadastrarCandidatos(GerenciamentoVotacao votacao) {
+    short input = 1;
+    while (input == 1) {
+      input = openMenuforPessoaCandidata(scanner);
+      if (input == 1) {
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String nome = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        short numeroCandidato = scanner.nextShort();
+        votacao.cadastrarPessoaCandidata(nome, numeroCandidato);
+      }
+    }
+  }
+
+  /**
+   * Metodo para cadastrar pessoas Eleitoras.
+   * @param votacao instancia da votação atual
+   * @author Murilo
+   */
+  private static void cadastrarEleitores(GerenciamentoVotacao votacao) {
+    short input = 1;
+    while (input == 1) {
+      input = openMenuforPessoaEleitora(scanner);
+      if (input == 1) {
+        System.out.println("Entre com o nome da pessoa eleitora:");
+        String nome = scanner.next();
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpfEleitor = scanner.next();
+        votacao.cadastrarPessoaEleitora(nome, cpfEleitor);
+      }
+    }
+  }
+
+  /**
+   * Metodo para inciar votacao.
+   * @param votacao instancia da votação atual
+   * @author Murilo
+   */
+  private static void iniciarVotação(GerenciamentoVotacao votacao) {
     short input = 0;
     while (input != 3) {
       input = openMenuforVotation(scanner);
@@ -36,72 +136,6 @@ public class Principal {
         }
       } else {
         // mostrar resultado parcial
-      }
-    }
-  }
-
-  private static short openMenuforPessoaCandidata(Scanner scanner) {
-    short inputNumber = 0;
-    System.out.println("Cadastrar pessoa candidata?");
-    String entrySystemString =
-        "1 - Sim\n2 - Não";
-    System.out.println(entrySystemString);
-    System.out.println("Entre com o número correspondente à opção desejada:");
-
-    inputNumber = scanner.nextShort();
-
-    return inputNumber;
-  }
-
-  private static short openMenuforPessoaEleitora(Scanner scanner) {
-    short inputNumber = 0;
-    System.out.println("Cadastrar pessoa eleitora?");
-    String entrySystemString =
-        "1 - Sim\n2 - Não";
-    System.out.println(entrySystemString);
-    System.out.println("Entre com o número correspondente à opção desejada:");
-
-    inputNumber = scanner.nextShort();
-
-    return inputNumber;
-  }
-
-  private static short openMenuforVotation(Scanner scanner) {
-    short inputNumber = 0;
-    System.out.println("Entre com o número correspondente à opção desejada:");
-    String entrySystemString =
-        "1 - Votar\n2 - Resultado Parcial\n3 - Finalizar Votação";
-        System.out.println(entrySystemString);
-
-    inputNumber = scanner.nextShort();
-
-    return inputNumber;
-  }
-
-  private static void cadastrarCandidatos(GerenciamentoVotacao votacao) {
-    short input = 1;
-    while (input == 1) {
-      input = openMenuforPessoaCandidata(scanner);
-      if (input == 1) {
-        System.out.println("Entre com o nome da pessoa candidata:");
-        String nome = scanner.next();
-        System.out.println("Entre com o número da pessoa candidata:");
-        short numeroCandidato = scanner.nextShort();
-        votacao.cadastrarPessoaCandidata(nome, numeroCandidato);
-      }
-    }
-  }
-
-  private static void cadastrarEleitores(GerenciamentoVotacao votacao) {
-    short input = 1;
-    while (input == 1) {
-      input = openMenuforPessoaEleitora(scanner);
-      if (input == 1) {
-        System.out.println("Entre com o nome da pessoa eleitora:");
-        String nome = scanner.next();
-        System.out.println("Entre com o cpf da pessoa eleitora:");
-        String cpfEleitor = scanner.next();
-        votacao.cadastrarPessoaEleitora(nome, cpfEleitor);
       }
     }
   }
